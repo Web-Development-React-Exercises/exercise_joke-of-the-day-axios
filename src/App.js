b6a1a6eb21360080d1aeaedbc1b45d6c03552bf5
PortalFan202 <anibaldonoso1@gmail.com>
2022-09-22 15:11:05 +0300

    Finished task

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+const URL = 'https://api.jokes.one/jod'
 
 function App() {
-  return (
-    <>
-    </>
+  const[title, setTitle] = useState('');
+  const[text, setText] = useState('');
+
+  useEffect( () => {
+    axios.get(URL)
+      .then(response => {
+        const joke = response.data.contents.jokes[0].joke;
+        setTitle(joke.title);
+        setText(joke.text);
+      }).catch (error => {
+        alert(error);
+      });
+  }
+  ,[]);
+
+  return ( 
+    <div>
+      <h1>Joke of the day</h1>
+      <h3>{title}</h3>
+      <p>{text}</p>
+    </div>
   );
 }
 
